475a452415dac85b3c4e0421b668adee76d41aa4
Chetan garg <chetan.garg36@gmail.com>
2019-05-31 15:17:15 +0530

    slider

---
+++ app/src/main/java/dev/ch8n/animations/material/slider/SliderViewActivity.kt
@@ -0,0 +1,13 @@
+package dev.ch8n.animations.material.slider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.ch8n.animations.R
+
+class SliderViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_slider_view)
+    }
+}
